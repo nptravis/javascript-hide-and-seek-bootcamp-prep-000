fcc7792e68eaafbf327c2568d4197ae62d65a8dd
Nic Travis <nptravis@yahoo.com>
2018-01-24 06:59:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector){
+  return document.querySelector(`${selector}`);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested').querySelector('.target');
+}
+
+function increaseRankBy(n){
+  var el = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (var i in el){
+    el[i].innerHTML = parseInt(el[i].innerHTML) + n;
+  }
+
+}
+
+function deepestChild(){
+  var el = document.getElementById('grand-node').querySelectorAll('div');
+  return el[el.length-1];
+}
